8106ec3684eef4553fd8ccfa49aaf6231ddda288
Usama-afzal7 <afzalusama77@gmail.com>
2024-01-15 21:21:14 +0500

    Fill Orders & Seed Exchange

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,36 @@
 async function main() {
-  //Fetch contracts to deploy
+  console.log(`Preparing deployment...\n`)
 
-  const Token = await ethers.getContractFactory("Token");
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory('Token')
+  const Exchange = await ethers.getContractFactory('Exchange')
 
-  //Deploy Contract
+  // Fetch accounts
+  const accounts = await ethers.getSigners()
 
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`Token deployed to: ${token.address}`);
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
+
+  // Deploy contracts
+  const dapp = await Token.deploy('SZAIBST', 'ZAB', '1000000')
+  await dapp.deployed()
+  console.log(`ZAB Deployed to: ${dapp.address}`)
+
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+  await mETH.deployed()
+  console.log(`mETH Deployed to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+  await mDAI.deployed()
+  console.log(`mDAI Deployed to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange Deployed to: ${exchange.address}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
